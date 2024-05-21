d9ae0473a51daf2f78c822134cc0546920bc8615
Ali <90032081+aliahadmd@users.noreply.github.com>
2024-05-21 22:56:57 +0600

    fix local server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 3000;
 
+// hello world
+app.get("/", (req, res)=>{
+    res.send("Hello World!");
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 })
